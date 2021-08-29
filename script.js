5a01afd56d43632458728b648e31a11dfc46aeec
Kyle Aldridge <thekylealdridge@gmail.com>
2021-08-29 16:30:21 -0400

    changed to allow up to 8 values

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
+'use strict';
 const preview = document.querySelector('#preview');
+const previewForm = document.querySelector("#previewForm");
 
-preview.addEventListener('click', (e) => {
+
+previewForm.addEventListener('focusin', (e) => {
   if(!e.target.dataset.border) return;
 
   let borderElement = e.target;
-  let borderType = borderElement.dataset.border;
+  let borderContainer = borderElement.closest('[data-border-container]');
+  if(!borderContainer) return;
+
   borderElement.addEventListener('input', (e) => {
     let val = +e.target.value;
     if(!/\d+/.test(val)) return;
-    preview.style['border'+borderType+'Radius'] = val + 'px';
+
+    let borderRadius = { left: 0, right: 0 };
+
+    let childrenInputs = borderContainer.querySelectorAll('[data-border]');
+    for(let child of childrenInputs) {
+      switch(child.dataset.border) {
+        case "horizontal":
+          borderRadius.horizontal = +child.value;
+          break;
+        case "vertical":
+          borderRadius.vertical= +child.value;
+          break;
+      }
+    }
+
+    preview.style['border'+borderContainer.dataset.borderContainer+'Radius'] = `${borderRadius.horizontal}px ${borderRadius.vertical}px`;
   });
 });
