ffdbc223e16b185b190368b8e971f7a672cd765b
Kyle Aldridge <thekylealdridge@gmail.com>
2021-08-29 18:30:15 -0400

    added copy button and github link

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 'use strict';
 const preview = document.querySelector('#preview');
 const previewForm = document.querySelector("#previewForm");
-
+const copy = document.querySelector(".copy__text");
+const copyButton = document.querySelector("#copy");
 
 previewForm.addEventListener('focusin', (e) => {
   if(!e.target.dataset.border) return;
@@ -11,23 +12,55 @@ previewForm.addEventListener('focusin', (e) => {
   if(!borderContainer) return;
 
   borderElement.addEventListener('input', (e) => {
-    let val = +e.target.value;
-    if(!/\d+/.test(val)) return;
-
-    let borderRadius = { left: 0, right: 0 };
+    let val = e.target.value;
+    if (!/\d+(px|em|rem|%){1}/g.test(val) && val) {
+      e.target.classList.add('error');
+      return;
+    }
+    e.target.classList.remove('error');
+    let borderRadius = { horizontal: 0, vertical: 0 };
 
     let childrenInputs = borderContainer.querySelectorAll('[data-border]');
     for(let child of childrenInputs) {
       switch(child.dataset.border) {
         case "horizontal":
-          borderRadius.horizontal = +child.value;
+          borderRadius.horizontal = (child.value) ? child.value : 0;
           break;
         case "vertical":
-          borderRadius.vertical= +child.value;
+          borderRadius.vertical = (child.value) ? child.value : 0;
           break;
       }
     }
 
-    preview.style['border'+borderContainer.dataset.borderContainer+'Radius'] = `${borderRadius.horizontal}px ${borderRadius.vertical}px`;
+    preview.style['border'+borderContainer.dataset.borderContainer+'Radius'] = `${borderRadius.horizontal} ${borderRadius.vertical}`;
+    
+    copy.querySelector('#' + borderContainer.dataset.borderContainer + '-horizontal').textContent = borderRadius.horizontal;
+    copy.querySelector('#' + borderContainer.dataset.borderContainer + '-vertical').textContent = borderRadius.vertical;
   });
+});
+
+copyButton.addEventListener('click', (e) => {
+  let r = document.createRange();
+  r.selectNode(copy);
+  window.getSelection().removeAllRanges();
+  window.getSelection().addRange(r);
+  document.execCommand('copy');
+  window.getSelection().removeAllRanges();
+
+  let newElem = document.createElement('div');
+  newElem.style.position = "absolute";
+  newElem.innerText = "Copied to clipboard!";
+  newElem.style.whiteSpace = "nowrap";
+  newElem.style.color = "#fff";
+  newElem.style.opacity = 1;
+  newElem.style.transition = "1s opacity";
+  copy.append(newElem);
+
+  let copyRect = document.querySelector(".copy").getBoundingClientRect();
+  newElem.style.top = copyRect.height + (newElem.clientHeight/2) + "px";
+  newElem.style.left = (copyRect.width / 2) - (newElem.clientWidth / 2) + "px";
+
+  setTimeout(() => { newElem.style.opacity = 0; newElem.remove(); }, 1500);
+
+  e.preventDefault();
 });
